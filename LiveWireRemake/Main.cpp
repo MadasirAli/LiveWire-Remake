134f82ad18ee90eeb3d80fe3e753627646d9e1ac
MadasirAli <madasirali672@gmail.com>
2024-09-29 08:56:10 +0500

    exception handler in live wire remake's main

--- LiveWireRemake/Main.cpp
+++ LiveWireRemake/Main.cpp
@@ -1,11 +1,25 @@
 #include <memory>
 
 #include "App.h"
+#include "BaghdadCore/BaghdadError.h"
 
 int main()
 {
     auto pApp = std::make_unique<LiveWireRemake::App>();
-    const auto returnCode = pApp->Run();
+    int returnCode = 0;
+    try
+    {
+        returnCode = pApp->Run();
+    }
+    catch (const BaghdadCore::BaghdadError& e)
+    {
+        e.Show();
+    }
+    catch (const std::exception& e)
+    {
+        BaghdadCore::BaghdadError error(e.what());
+        error.Show();
+    }
 
     return returnCode;
 }
