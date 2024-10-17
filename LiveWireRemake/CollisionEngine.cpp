73240fbfca5b2dae2b616cff20e0ad0c53d3dead
PlantaeGames <plantaegames@gmail.com>
2024-10-17 07:50:31 +0500

    collision engine done,

--- LiveWireRemake/CollisionEngine.cpp
+++ LiveWireRemake/CollisionEngine.cpp
@@ -62,22 +62,81 @@ void CollisionEngine::OnLateUpdate(std::weak_ptr<Entity>& pEntity)
 	// handling overlapp of collisions
 	for (auto& pCollisions : collisionsList)
 	{
-		for (auto& pCollision : pCollisions)
+		const auto collisionCount = pCollisions.size() - 1;
+		if (collisionCount == 0)
+			continue;	// no collisions with this entity
+
+		auto pMe = pCollisions[0].lock();
+
+		// overlapping is intentional
+		if (pMe->trigger)
+			continue;
+
+		// counter displacing overlap
+		using namespace DirectX;
+
+		auto pMyTransform = pMe->GetParent().lock()->GetTransform();
+
+		unsigned int overlapAccumulation = 0u;
+		unsigned int overlapPartioners = 0u;
+		// getting overlap arthimetic accumulation
+		for (auto i = 1; i < collisionCount; ++i)
 		{
-			const auto partioners = pCollisions.size() - 1;
-			if (partioners == 0)
-				continue;	// no collisions with this entity
+			auto pOther = pCollisions[i].lock();
+			auto pOtherTransform = pOther->GetParent().lock()->GetTransform();
 
-			auto pMe = pCollision.lock();
+			// getting frame displacement
+			auto frameDisplacement = DirectX::XMVectorSubtract
+				(DirectX::XMVectorSet(pOther->_prePosition.x, pOther->_prePosition.y, pOther->_prePosition.z, 0),
+				DirectX::XMVectorSet(pOtherTransform.position.x, pOtherTransform.position.y, pOtherTransform.position.z, 0));
 
-			// overlapping is intentional
-			if (pMe->trigger)
+			// if this collider does not contributed actions in overlap.
+			if (XMVectorGetX(XMVectorEqual(XMVector3LengthEst(frameDisplacement), XMVectorZero())) > 0u)
+			{
 				continue;
+			}
 
-			// counter displacing overlap
-			using namespace DirectX;
+			// getting overlap
+			auto bothDisplacement = DirectX::XMVectorSubtract
+				(DirectX::XMVectorSet(pOtherTransform.position.x, pOtherTransform.position.y, pOtherTransform.position.z, 0),
+				DirectX::XMVectorSet(pMyTransform.position.x, pMyTransform.position.y, pMyTransform.position.z, 0));
+			auto bothDistance = XMVector3Length(bothDisplacement);
+			auto bothDir = XMVectorDivide(bothDisplacement, bothDistance);
 
-			
+			auto myEdge = XMVectorAdd(
+				XMVectorSet(pMyTransform.position.x, pMyTransform.position.y, pMyTransform.position.z, 0),
+				XMVectorMultiply(bothDir, XMVectorSet(pMe->radius, pMe->radius, pMe->radius, pMe->radius)));
+
+			auto otherEdge = XMVectorAdd(
+				XMVectorSet(pOtherTransform.position.x, pOtherTransform.position.y, pOtherTransform.position.z, 0),
+				XMVectorMultiply(XMVectorNegate(bothDir), XMVectorSet(pMe->radius, pMe->radius, pMe->radius, pMe->radius)));
+
+			// getting edge overlap displacement
+			auto overlap = XMVectorSubtract(otherEdge, myEdge);
+
+			overlapAccumulation += XMVectorGetX(XMVector3Length(overlap));
+			++overlapPartioners;
 		}
+
+		// calculating anti overlap
+		// getting frame displacement
+		auto frameDisplacement = DirectX::XMVectorSubtract
+			(DirectX::XMVectorSet(pMe->_prePosition.x, pMe->_prePosition.y, pMe->_prePosition.z, 0),
+			DirectX::XMVectorSet(pMyTransform.position.x, pMyTransform.position.y, pMyTransform.position.z, 0));
+
+		auto frameDistance = XMVector3Length(frameDisplacement);
+		auto frameDir = XMVectorDivide(frameDisplacement, frameDistance);
+
+		// getting contributed magnitude
+		const auto contribution = overlapAccumulation / (float)overlapPartioners;
+
+		auto antiOverlap = XMVectorMultiply(-frameDir, XMVectorSet(contribution, contribution, contribution, contribution));
+		
+		// applying anti overlap
+		auto newPosition = XMVectorAdd(
+			XMVectorSet(pMyTransform.position.x, pMyTransform.position.y, pMyTransform.position.z, 0),
+			antiOverlap);
+
+		pMyTransform.position = XMFLOAT3(XMVectorGetX(newPosition), XMVectorGetY(newPosition), XMVectorGetZ(newPosition));
 	}
 }
