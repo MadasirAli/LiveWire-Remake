08c204c3f5af2a264c44e80bc27d5d2448a0fb85
PlantaeGames <plantaegames@gmail.com>
2024-10-20 11:56:00 +0500

    tile texture

--- LiveWireRemake/MeshRenderer.cpp
+++ LiveWireRemake/MeshRenderer.cpp
@@ -80,7 +80,7 @@ MeshRenderer::MeshRenderer()
 	// binding default texture
 	auto defaultTexture = renderer.GetTextureBuilder()
 		.Clear()
-		.FromFile("Resources\\Textures\\landscape.bmp")
+		.FromFile("Resources\\Textures\\tile.bmp")
 		.ViewFlag(BaghdadCore::Resource::View::Type::SRV)
 		.Build();
 
